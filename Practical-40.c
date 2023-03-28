1cb93555be1a9dbac86bb4d280eddc46df3e5d4c
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-28 09:02:43 +0530

    Reverse a String

---
+++ Practical-40.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+char str[50],rev[50];
+int i,j;
+printf("\n  Enter String to Reverse : ");
+scanf("%[^\n]s",str);
+
+for(i=0;str[i]!='\0';i++)
+{
+ 
+}
+i--;
+
+for(j=0;i>=0;j++,i--)
+{
+ rev[j]=str[i];
+}
+rev[j]='\0';
+
+printf("\n Reverse String = %s",rev);
+return 0;
+}
