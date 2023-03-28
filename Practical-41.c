e2c3c0e141dde15dc1c641195b5cf5e2e345f657
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-28 09:04:59 +0530

    Convert String into upper case

---
+++ Practical-41.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+char str[50];
+int i;
+printf("\n  Enter String : ");
+scanf("%[^\n]s",str);
+
+for(i=0;str[i]!='\0';i++)
+{
+ if(str[i]>='a' && str[i]<='z')
+ {
+  str[i]=str[i]-32;
+ }
+}
+printf("\n Upper Case String = %s",str);
+return 0;
+}
